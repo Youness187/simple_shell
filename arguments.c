d032954102e54a11e1d1406056f57c142d675e1c
younesse el-mouden <dayouness187@gmail.com>
2023-05-24 15:09:50 +0000

    spase

--- arguments.c
+++ arguments.c
@@ -9,34 +9,21 @@ char **arguments(char *buffer)
 {
 	char **arg;
 	char *token;
-	char *buffer_copy;
 	int len_buffer = 0, i;
-	unsigned long int len = lenstr(buffer);
+	int len = lenstr(buffer);
 
-	buffer_copy = malloc(sizeof(char) * len);
-	if (buffer_copy == NULL)
-	{
-		return (NULL);
-	}
-	_strcpy(buffer_copy, buffer);
-	token = strtok(buffer_copy, " \n\t\r\a\v");
-	if (token == NULL)
-	{
-		free(buffer_copy);
-		return (NULL);
-	}
-	for (len_buffer = 0; token; len_buffer++)
-	{
-		token = strtok(NULL, " \n\t\r\a\v");
-	}
-	free(buffer_copy);
-	arg = malloc(sizeof(char *) * len_buffer + 1);
+	arg = malloc(sizeof(char *) * 200);
 
 	if (arg == NULL)
 	{
 		return (NULL);
 	}
 	token = strtok(buffer, " \n\t\r\a\v");
+	if (token == NULL)
+	{
+		free(arg);
+		return (NULL);
+	}
 	for (i = 0; token; i++)
 	{
 		arg[i] = malloc(sizeof(char) * lenstr(token) + 1);
@@ -45,6 +32,5 @@ char **arguments(char *buffer)
 		token = strtok(NULL, " \n\t\r\a\v");
 	}
 	arg[i] = NULL;
-	free(token);
 	return (arg);
 }
